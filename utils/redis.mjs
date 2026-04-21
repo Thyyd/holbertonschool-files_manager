4a9109c2253e635a00f185c2e702e8aa7d6745e5
Thyyd <roastingblade@gmail.com>
2026-04-21 15:50:27 +0200

    Correction problèmes ESLint!

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,42 +1,42 @@
-import redis from "redis";
+import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    // Constructeur
-    constructor() {
-        this.client = redis.createClient();
+  // Constructeur
+  constructor() {
+    this.client = redis.createClient();
 
-        this.client.on("error", (error) => {
-            console.log(`Redis client not connected to the server: ${error.message}`)
-        });
+    this.client.on('error', (error) => {
+        console.log(`Redis client not connected to the server: ${error.message}`)
+    });
 
-        // Gestion des promisify
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.expireAsync = promisify(this.client.expire).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+    // Gestion des promisify
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.expireAsync = promisify(this.client.expire).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    // Méthode isAlive
-    isAlive() {
-        return this.client.connected;
-    }
+  // Méthode isAlive
+  isAlive() {
+    return this.client.connected;
+  }
 
-    // Méthode get
-    async get(key) {
-        return this.getAsync(key);
-    }
+  // Méthode get
+  async get(key) {
+    return this.getAsync(key);
+  }
 
-    // Méthode set
-    async set(key, value, duration) {
-        await this.setAsync(key, value);
-        await this.expireAsync(key, duration);
-    }
+  // Méthode set
+  async set(key, value, duration) {
+    await this.setAsync(key, value);
+    await this.expireAsync(key, duration);
+  }
 
-    // Méthode del
-    async del(key) {
-        await this.delAsync(key);
-    }
+  // Méthode del
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
