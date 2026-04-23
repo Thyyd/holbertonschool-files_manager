11ad0a01039206f26e5529ab34873e13e80a0b18
Thyyd <roastingblade@gmail.com>
2026-04-23 17:12:56 +0200

    MàJ des fichiers de la task 3 : Correction problèmes ESLint!

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,28 +4,27 @@ import db from '../utils/db';
 const UsersController = {
   postNew: async (req, res) => {
     // Récupération de l'email et du pwd
-    const email = req.body.email;
-    const pwd = req.body.password;
+    const { email, password } = req.body;
 
     // Vérification de l'email et du pwd
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
-    if (!pwd) {
+    if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
 
     // Vérifie que l'email n'existe pas déjà dans la DB
-    const emailExists = await db.client.db(db.database).collection('users').findOne({ email: email });
+    const emailExists = await db.client.db(db.database).collection('users').findOne({ email });
     if (emailExists) {
       return res.status(400).json({ error: 'Already exist' });
     }
 
     // Hashage du pwd
-    const hashedPwd = crypto.createHash('sha1').update(pwd).digest('hex');
+    const hashedPwd = crypto.createHash('sha1').update(password).digest('hex');
 
-    const newId = await db.client.db(db.database).collection('users').insertOne({ email: email, password: hashedPwd });
-    return res.status(201).json({ id: newId.insertedId, email: email });
+    const newId = await db.client.db(db.database).collection('users').insertOne({ email, password: hashedPwd });
+    return res.status(201).json({ id: newId.insertedId, email });
   },
 };
 
