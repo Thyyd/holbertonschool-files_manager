04fec128a54cec160cd95263edb6a18b24c83407
Thyyd <roastingblade@gmail.com>
2026-04-23 16:55:26 +0200

    Ajout des fichiers de la task 3 : Création d'un endpoint pour ajouter un nouvel utilisateur.

---
+++ controllers/UsersController.js
@@ -0,0 +1,32 @@
+import db from '../utils/db';
+import crypto from 'crypto';
+
+const UsersController = {
+  postNew: async (req, res) => {
+    // Récupération de l'email et du pwd
+    const email = req.body.email;
+    const pwd = req.body.password;
+
+    // Vérification de l'email et du pwd
+    if (!email) {
+      return res.status(400).send('Missing email');
+    }
+    if(!pwd) {
+      return res.status(400).send('Missing password');
+    }
+
+    // Vérifie que l'email n'existe pas déjà dans la DB
+    const emailExists = await db.client.db(db.database).collection('users').findOne({ email: email });
+    if (emailExists) {
+      return res.status(400).send('Already exist');
+    }
+
+    // Hashage du pwd
+    const hashedPwd = crypto.createHash('sha1').update(pwd).digest('hex');
+
+    const newId = await db.client.db(db.database).collection('users').insertOne({ email: email, password: hashedPwd });
+    return res.status(201).json({ id: newId.insertedId, email: email });
+  },
+};
+
+export default UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 // Initialisation d'un Routeur Express
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 // Création des routes GET
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
 
 export default router;
