de79a990424edfe4cc6f2d06d158fb07ee0f51cd
Thyyd <roastingblade@gmail.com>
2026-04-23 14:03:21 +0200

    MàJ des fichiers de la task 2 : Correction problèmes ESLint!

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,14 +3,14 @@ import redis from '../utils/redis.mjs';
 
 const AppController = {
   getStatus: (req, res) => {
-    res.status(200).json({"redis": redis.isAlive(), "db": db.isAlive()});
+    res.status(200).json({ "redis": redis.isAlive(), "db": db.isAlive() });
   },
 
   getStats: async (req, res) => {
     const nbUsers = await db.nbUsers();
     const nbFiles = await db.nbFiles();
 
-    res.status(200).json({"users": nbUsers, "files": nbFiles});
+    res.status(200).json({ "users": nbUsers, "files": nbFiles });
   }
 };
 
