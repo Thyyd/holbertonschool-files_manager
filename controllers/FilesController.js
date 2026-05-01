983a48efdef5489533f08c5103d527ce4bf13ea4
Thyyd <roastingblade@gmail.com>
2026-05-01 17:14:12 +0200

    Ajout des fichiers de la task 6 : Création de 2 endpoints rendre public ou privé un file.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -155,6 +155,68 @@ const FilesController = {
       return res.status(500).json({ error: 'Internal error' });
     }
   },
+
+  // Méthode putPublish
+  putPublish: async (req, res) => {
+    const { id } = req.params.id;
+
+    // Récupération de l'user Redis id
+    const xTokenHeader = req.header('x-token');
+    const key = `auth_${xTokenHeader}`;
+    const userId = await redis.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      const objectId = new ObjectId(id);
+
+      const fileToUnpublish = await db.database.collection('files').findOneAndUpdate(
+        { _id: objectId, userId: new ObjectId(userId) },
+        { $set: {isPublic: true} },
+        { returnDocument: 'after' }
+      );
+      if (!fileToUnpublish) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      return res.status(200).json(fileToUnpublish);
+    }
+    catch (_err) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+  },
+
+  // Méthode putUnpublish
+  putUnpublish: async (req, res) => {
+    const { id } = req.params.id;
+
+    // Récupération de l'user Redis id
+    const xTokenHeader = req.header('x-token');
+    const key = `auth_${xTokenHeader}`;
+    const userId = await redis.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      const objectId = new ObjectId(id);
+
+      const fileToPublish = await db.database.collection('files').findOneAndUpdate(
+        { _id: objectId, userId: new ObjectId(userId) },
+        { $set: {isPublic: false} },
+        { returnDocument: 'after' }
+      );
+      if (!fileToPublish) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      return res.status(200).json(fileToPublish);
+    }
+    catch (_err) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+  },
 };
 
 export default FilesController;
